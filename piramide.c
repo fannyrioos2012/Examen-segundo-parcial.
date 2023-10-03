c8a47bd83795a23853f45ed21f1e13c957eac555
fannyrioos2012 <142334991+fannyrioos2012@users.noreply.github.com>
2023-10-03 09:50:30 -0600

    Add files via upload

---
+++ piramide.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, lineas;
+
+    printf("Mostrar numero de lineas en la piramide:) : ");
+    scanf("%d", &lineas);
+
+    for (i = 1; i <= lineas; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
